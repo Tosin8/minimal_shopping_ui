b48a3336d3721cd80d2d2da359693a117dceafe6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-08-23 22:53:12 +0100

    Updating Main.dart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Home_Page(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// ignore: camel_case_types
+class Home_Page extends StatefulWidget {
+  const Home_Page({super.key});
 
   @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+// ignore: camel_case_types
+class _Home_PageState extends State<Home_Page> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: ListView(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 20),
+                child: Image.asset('assets/images/nike_logo.png',
+                    width: 20, height: 30)),
+          )
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    ));
   }
 }
