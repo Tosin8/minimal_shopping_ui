44a1da3ddf310cd0d2122393f8655069ee9465c7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-08-26 00:01:56 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,46 +23,69 @@ class _Home_PageState extends State<Home_Page> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      child: ListView(
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-                padding: const EdgeInsets.only(top: 20, bottom: 20),
-                child: Image.asset('assets/images/nike_logo.png',
-                    width: 20, height: 30)),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: ListView(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 20),
+                    child: Image.asset('assets/images/nike_logo.png',
+                        width: 20, height: 30)),
+              ),
+              const SizedBox(height: 15),
+              const ProductCard(
+                imgUrl: 'assets/images/nike_airforce_w.png',
+                title: 'Nike Airforce W',
+                color_product: Color(0xFFEF6C00),
+                product_price: '\$120.00',
+              ),
+              const SizedBox(height: 15),
+              const ProductCard(
+                imgUrl: 'assets/images/nike_x_sbdung_green_lobster.png',
+                title: 'Nike X Sbding Green',
+                color_product: Color(0xFF0277BD),
+                product_price: '\$80.00',
+              ),
+              const SizedBox(height: 15),
+              const ProductCard(
+                imgUrl: 'assets/images/nike_zoom_pegasus_33-light_blue.png',
+                title: 'Nike Pegasus Blue',
+                color_product: Color(0xFF512DA8),
+                product_price: '\$50.00',
+              ),
+              const SizedBox(height: 15),
+              const ProductCard(
+                imgUrl: 'assets/images/nike_zoom_pegasus_33_light_pink.png',
+                title: 'Nike Pegaus Pink',
+                color_product: Color(0xFFF9A825),
+                product_price: '\$56.00',
+              ),
+            ],
           ),
-          const SizedBox(height: 15),
-          const ProductCard(
-            imgUrl: 'assets/images/nike_airforce_w.png',
-            title: 'Nike Airforce W',
-            color_product: Color(0xFFEF6C00),
-            product_price: '\$120.00',
-          ),
-          const SizedBox(height: 15),
-          const ProductCard(
-            imgUrl: 'assets/images/nike_x_sbdung_green_lobster.png',
-            title: 'Nike X Sbding Green',
-            color_product: Color(0xFF0277BD),
-            product_price: '\$80.00',
-          ),
-          const SizedBox(height: 15),
-          const ProductCard(
-            imgUrl: 'assets/images/nike_zoom_pegasus_33-light_blue.png',
-            title: 'Nike Pegasus Blue',
-            color_product: Color(0xFF512DA8),
-            product_price: '\$50.00',
-          ),
-          const SizedBox(height: 15),
-          const ProductCard(
-            imgUrl: 'assets/images/nike_zoom_pegasus_33_light_pink.png',
-            title: 'Nike Pegaus Pink',
-            color_product: Color(0xFFF9A825),
-            product_price: '\$56.00',
-          ),
-        ],
-      ),
-    ));
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 0,
+          iconSize: 20,
+          selectedFontSize: 20,
+          selectedItemColor: Colors.amberAccent,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          selectedIconTheme: const IconThemeData(color: Colors.amberAccent, size: 40), 
+          backgroundColor: Colors.white10,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+          ],
+        ));
   }
 }
